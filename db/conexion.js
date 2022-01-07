108ba394de4944522c9f8be7d8c96a593c5d9fc4
GabyLudewig <64235704+GabyLudewig@users.noreply.github.com>
2022-01-07 17:05:10 -0600

    css

--- db/conexion.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('red_social_tecla', 'root', null, {
-    host: 'localhost',
-    dialect: 'mysql'
-})
-
-module.exports = sequelize;
