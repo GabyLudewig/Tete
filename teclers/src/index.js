89222a334be24aec8c9e6583ee86f2ffb926693f
mane1807 <hernandezgutierrezjuan7@gmail.com>
2022-01-21 12:43:14 -0600

    inicio-react

--- teclers/src/index.js
+++ teclers/src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import{global}from './global/global'
-//import {Counter} from './components/Counter/Counter';
+
 import {Nav} from './components/Nav/Nav';
-import{Inicio}from './components/Inicio/Inicio'
-import{Quotes}from './components/Quotes/Quotes'
-//import{Buspub}from './components/Inicio/Buspub';
+import{Quotes}from './components/Quotes//Quotes'
+
+
 ReactDOM.render(
   <>
-    {/*<Buspub/>*/}
+    
     <Nav />
-    <Inicio/>
+  
     <Quotes quotesDB={global.quotesDB} />
-    {/*<Counter/>*/}
+     
   </>,
   document.getElementById('root')
 );
