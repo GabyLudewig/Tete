01c79c7fafa0c2a297f3b777b6f7518fdc557f7d
mane1807 <hernandezgutierrezjuan7@gmail.com>
2022-03-04 10:34:14 -0600

    testing correjido

--- backend/test/controller.quotes.js
+++ backend/test/controller.quotes.js
@@ -2,11 +2,11 @@ const quotesController = require("../controller/quotes");
 const expect = require("chai").expect;
 
 
-describe("Testing quotesController",()=>{
-    describe("check add funtion",()=>{
-        it("check return value:",()=>{
-            result= quotesController.add(3,7);
+describe("Testing quotesController", () => {
+    describe("check add funtion", () => {
+        it("check return value:", () => {
+           let  result= quotesController.add(3,7);
             expect(result).to.equal(10);
-        })
-    })
-})
+        });
+    });
+});
